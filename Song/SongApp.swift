5474d6d87998228d17bf1f2d1520a0b2e37ed4ca
tofishi <info.tofishi@gmail.com>
2024-08-14 20:47:10 +0530

    Initial Commit

---
+++ Song/SongApp.swift
@@ -0,0 +1,17 @@
+//
+//  SongApp.swift
+//  Song
+//
+//  Created by Mohammad Tofik Sheikh on 14/08/24.
+//
+
+import SwiftUI
+
+@main
+struct SongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
